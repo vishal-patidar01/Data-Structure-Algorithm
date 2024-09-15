bb04e9ea495964bc4e60a3a0ed57bc923ceb82e3
vishal patidar <vishalsuman8770@gmail.com>
2024-09-15 17:13:41 +0530

    Loops

---
+++ Loops/Do_while.java
@@ -0,0 +1,9 @@
+public class Do_while {
+    public static void main(String[] args) {
+        int i = 0;
+        do {
+            System.out.println("count : "+ i);
+            i++;
+        }while(i<=10);
+    }
+}
---
+++ Loops/fouthloop.java
@@ -0,0 +1,8 @@
+public class fouthloop {
+    public static void main(String[] args) {
+        int number[] = {1, 2, 3, 4, 5};
+        for(int num : number) {
+            System.out.println(num);
+        }
+    }
+}
